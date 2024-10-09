cf17434c95f701c8ecac19d0bb700dd2f55dfb24
Your Name <you@example.com>
2024-10-09 17:03:56 +0300

    olioita lisätty

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@ let paddleHeight = 30;
 let paddleYPosition = 400;
 let ghostImage;
 
+let ghostOlio;
+let ghostOlio2;
+
 function preload(){
     ghostImage = loadImage("Images/ghost.png");
 }
@@ -10,12 +13,15 @@ function preload(){
 function setup(){
     createCanvas(windowWidth,windowHeight);
     background("black");
+    ghostOlio = new Ghost();
+    ghostOlio2 = new Ghost();
 }
 
 function draw(){
     background("black");
     draw_paddle();
-    
+    ghostOlio.Move();
+    ghostOlio2.Move();
 }
 
 function draw_paddle(){
@@ -26,16 +32,20 @@ function draw_paddle(){
 class Ghost{
     constructor(){
         this.x = 0;
-        this.y = 0;
+        this.y = random(0,50);
         this.width = 30;
         this.height = 30;
-        this.xSpeed = 1;
+        this.xSpeed = random(1,4);
         this.ySpeed = -1;
         this.gravity = 0.05;
     }
     
     Move(){
-        
+        this.x = this.x + this.xSpeed;
+
+        this.ySpeed = this.ySpeed + this.gravity;
+
+        this.y = this.y + this.ySpeed;
         image(ghostImage,this.x,this.y,this.width,this.height);
         
     }
